d45310669e8fcd8d853cc633086f1a617985b4a3
Diamondk1 <dixaic@gmail.com>
2023-11-14 02:10:46 -0800

    workingdirectory builtin

---
+++ wkdir_buitins.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * _cdir - a function that changes the working directory
+ * @arg : arguments
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int _cdir(char **arg)
+{
+	char *nw_path = argv[1];
+	char *cr_path;
+
+	if (nw_path == NULL)
+		nw_path = getenv("HOME");
+	else
+	{
+		switch (nw_path[0])
+		{
+			case '~':
+				nw_path = getenv("HOME");
+				break;
+			case '_':
+				nw_path = getenv("PRPWD");
+				break;
+			default:
+				break;
+		}
+	}
+		if (chdir(nw_path) != 0)
+		{
+			perror("failed");
+				return (1);
+		}
+			cr_path = getcwd(NULL, 0);
+			setenv("CWD", cr_path, 1);
+			free(cr_path);
+
+			return (0);
+}
